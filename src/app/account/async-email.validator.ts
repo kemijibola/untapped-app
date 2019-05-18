2c6ea9d439f5b9c71ebf98edda6e40fd89fc8936
kemijibzz <kemijibbola@gmail.com>
2019-05-18 22:12:25 +0100

    non

--- src/app/account/async-email.validator.ts
+++ src/app/account/async-email.validator.ts
@@ -1,11 +1,8 @@
 import { AuthService } from '../services/auth.service';
 import { FormControl } from '@angular/forms';
-import { timer, of, pipe, Observable } from 'rxjs';
-import { switchMap, take } from 'rxjs/operators/';
+import { timer, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators/';
 import { map } from 'rxjs/operators/';
-import { Store, select } from '@ngrx/store';
-import * as fromApp from '../store/app.reducers';
-import * as AuthActions from './store/auth.actions';
 
 export function emailAsyncValidator(time: number = 500, authService: AuthService) {
   return (input: FormControl): Observable<any> | Promise<any> => {
