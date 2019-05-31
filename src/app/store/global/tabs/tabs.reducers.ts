5ddeeafb921d8ee025033cdf4c56c625c6fb4065
kemijibzz <kemijibbola@gmail.com>
2019-05-31 09:20:21 +0100

    changed tab store to manage all app tabs

--- src/app/store/global/tabs/tabs.reducers.ts
+++ src/app/store/global/tabs/tabs.reducers.ts
@@ -1,39 +1,53 @@
-import { Tab } from 'src/app/models';
+import { AppTab, Tab } from 'src/app/models';
 import * as TabsAction from './tabs.actions';
 
 export interface FeatureState {
     tabs: State;
 }
 export interface State {
-    tabs: Tab[];
-    selectedTab: Tab;
+    tabs: AppTab[];
 }
 
 const initialState: State = {
-    tabs: [],
-    selectedTab: null
+    tabs: []
 };
 
 export function TabsReducers(state = initialState, action: TabsAction.TabsAction) {
     switch (action.type) {
         case TabsAction.UPDATE_TAB:
-            action.payload.tab.active = true;
-            const tab = state.tabs[action.payload.index];
-            const updatedTab = {
-                ...tab,
-                ...action.payload.tab
-            };
-            const tabs = [...state.tabs];
-            tabs.forEach(x => x.active = false);
-            tabs[action.payload.tab.index] = updatedTab;
+        const tabByBame = state.tabs.filter(x => x.name === action.payload.name)[0];
+        const selectedTab = tabByBame.tabs[action.payload.tabIndex];
+        const updateObj = {
+            index: selectedTab.index,
+            title: selectedTab.title,
+            tag: selectedTab.tag,
+            active: true
+        };
+        selectedTab.active = true;
+        const updateTab = {
+            ...selectedTab,
+            ...updateObj
+        };
+        tabByBame.tabs.forEach(x => x.active = false);
+        tabByBame.tabs[action.payload.tabIndex] = updateTab;
+        return {
+            ...state,
+            tabs: [tabByBame]
+        };
+        case TabsAction.ADD_TAB:
             return {
                 ...state,
-                tabs: [...tabs]
+                tabs: [...state.tabs, action.payload.tab]
             };
         case TabsAction.ADD_TABS:
             return {
                 ...state,
-                tabs: action.payload.tabs
+                tabs: [...state.tabs, ...action.payload.tabs]
+            };
+        case TabsAction.DESTROY_TAB:
+            return {
+                ...state,
+                tabs: []
             };
         default:
             return state;
--- src/app/talent/talent.component.ts
+++ src/app/talent/talent.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Tab } from '../models';
+import { Tab, AppTab } from '../models';
 import { Store } from '@ngrx/store';
 import * as fromTabs from '../store/app.reducers';
 import * as TabsAction from '../store/global/tabs/tabs.actions';
@@ -10,20 +10,40 @@ import * as TabsAction from '../store/global/tabs/tabs.actions';
   templateUrl: './talent.component.html',
   styleUrls: ['./talent.component.css']
 })
-export class TalentComponent implements OnInit {
-  tabs: Tab[];
+export class TalentComponent implements OnInit, OnDestroy {
+  tab: AppTab;
+  tab2: AppTab;
+
+  componentName = 'Talent';
 
   constructor(private store: Store<fromTabs.AppState>) {
-    this.tabs = [
-      { index: 0, title: 'Profile', tag: 'profile', active: false },
-      {index: 1, title: 'Portfolio', tag: 'portfolio', active: false },
-      {index: 2, title: 'Settings', tag: 'settings', active: false }
-    ];
+
+    this.tab = {
+      name: this.componentName,
+      tabs: [
+        { index: 0, title: 'Profile', tag: 'profile', active: false },
+        { index: 1, title: 'Portfolio', tag: 'portfolio', active: false },
+        { index: 2, title: 'Settings', tag: 'settings', active: false }
+      ]
+    };
+
+    this.tab2 = {
+      name: 'Second',
+      tabs: [
+        { index: 0, title: 'Gigs', tag: 'gigs', active: false },
+        { index: 1, title: 'Sent Gigs', tag: 'sentgigs', active: false }
+      ]
+    };
   }
 
   ngOnInit() {
     // set up tabs with default properties
-    this.store.dispatch(new TabsAction.AddTabs({tabs: this.tabs}));
+    this.store.dispatch(new TabsAction.AddTab({tab: this.tab}));
+    this.store.dispatch(new TabsAction.AddTab({tab: this.tab2}));
+  }
+
+  ngOnDestroy() {
+    this.store.dispatch(new TabsAction.DestroyTab({name: this.componentName}));
   }
 
 }
