8844793c7cd918843125f3c7252e2b5700078ed2
kemijibzz <kemijibbola@gmail.com>
2020-06-28 14:11:06 +0100

    user general audio

--- src/app/shared/up-categery-search/up-categery-search.component.ts
+++ src/app/shared/up-categery-search/up-categery-search.component.ts
@@ -3,6 +3,7 @@ import { Store, select } from "@ngrx/store";
 import * as fromApp from "../../store/app.reducers";
 import { ICategory, OrderedCategory } from "src/app/interfaces";
 import * as fromCategory from "./../store/category/category.reducers";
+import { UUID } from "angular2-uuid";
 
 @Component({
   selector: "app-up-categery-search",
@@ -14,17 +15,25 @@ export class UpCategerySearchComponent implements OnInit {
   selectedCategoryIndex = 0;
   index = 0;
   categories: OrderedCategory[] = [];
+  allSelected: boolean = true;
   constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
     this.store
       .pipe(select(fromCategory.selectCategories))
       .subscribe((val: ICategory[]) => {
+        // // val.unshift({
+        // //   _id: "12346",
+        // //   name: "All",
+        // // });
+
         this.reOrderCategories(3, val);
+        this.orderedCategories[0].selected = true;
       });
   }
 
   onNext() {
+    // this.reOrderCategories(3, this.orderedCategories);
     this.store
       .pipe(select(fromCategory.selectCategories))
       .subscribe((val: ICategory[]) => {
@@ -35,8 +44,21 @@ export class UpCategerySearchComponent implements OnInit {
   }
 
   onSelectCategory(index: number) {
+    if (index === -1) {
+      this.allSelected = !this.allSelected;
+      this.orderedCategories = this.orderedCategories.map(
+        (x: OrderedCategory, i: number) => {
+          return Object.assign({}, x, {
+            selected: false,
+          });
+        }
+      );
+      return;
+    }
+
     this.orderedCategories = this.orderedCategories.map(
       (x: OrderedCategory, i: number) => {
+        this.allSelected = false;
         return Object.assign({}, x, {
           selected: i == index ? true : false,
         });
--- src/app/user/portfolio/portfolio-general/portfolio-general.component.ts
+++ src/app/user/portfolio/portfolio-general/portfolio-general.component.ts
@@ -2,7 +2,25 @@ import { Store, select } from "@ngrx/store";
 import * as fromUser from "../../user.reducers";
 import { Component, OnInit } from "@angular/core";
 import * as fromMediaPreview from "../../store/portfolio/media/media-preview.reducers";
-import { GeneralPreview } from "src/app/interfaces";
+import {
+  GeneralPreview,
+  MediaType,
+  AppModal,
+  ModalDisplay,
+  ModalViewModel,
+  IModal,
+} from "src/app/interfaces";
+import {
+  fetchAudioArt,
+  fetchImageObjectFromCloudFormation,
+  fetchNoMediaDefaultImage,
+  fetchVideoArt,
+} from "src/app/lib/Helper";
+import { ImageFit, ImageEditRequest } from "src/app/interfaces/media/image";
+import * as fromApp from "../../../store/app.reducers";
+import * as ModalsActions from "../../../shared/store/modals/modals.actions";
+import * as PortfolioActions from "../../store/portfolio/portfolio.actions";
+import * as fromModal from "../../../shared/store/modals/modals.reducers";
 
 @Component({
   selector: "app-portfolio-general",
@@ -10,32 +28,121 @@ import { GeneralPreview } from "src/app/interfaces";
   styleUrls: ["./portfolio-general.component.css"],
 })
 export class PortfolioGeneralComponent implements OnInit {
+  componentModal: AppModal;
   generalPreviews: GeneralPreview[] = [];
+  editParams: ImageEditRequest = {
+    edits: {
+      resize: {
+        width: 94,
+        height: 65,
+        fit: ImageFit.fill,
+      },
+      grayscale: false,
+    },
+  };
 
-  constructor(private userStore: Store<fromUser.UserState>) {}
+  constructor(
+    public store: Store<fromApp.AppState>,
+    private userStore: Store<fromUser.UserState>
+  ) {}
   ngOnInit() {
+    this.store
+      .pipe(select(fromModal.selectCurrentModal))
+      .subscribe((val: AppModal) => {
+        if (val) {
+          this.componentModal = { ...val };
+        }
+      });
     this.userStore
       .pipe(select(fromMediaPreview.selectUserGeneralPreviews))
       .subscribe((val: GeneralPreview[]) => {
-        console.log(val);
-        this.generalPreviews = val;
-        // this.userAudiosLength = val.length;
-        // if (val.length > 0) {
-        //   this.setAlbumCovers();
-        // }
+        if (val.length > 0) {
+          this.setMedia(val);
+        }
       });
   }
 
-  setMedia() {
-    
+  setMedia(previews: GeneralPreview[]) {
+    this.generalPreviews = previews.map((x) => {
+      const mediaType = x.mediaType.toUpperCase();
+      switch (mediaType) {
+        case MediaType.AUDIO:
+          x = this.setAudioAlbumCover(x);
+          break;
+        case MediaType.IMAGE:
+          x = this.setImageAlbumCover(x);
+          break;
+        case MediaType.VIDEO:
+          x = this.setVideoAlbumCover(x);
+          break;
+        default:
+          break;
+      }
+      return x;
+    });
   }
-  //   getVideoAlbumCover()
-  // getImageAlbumCover()
-  // getAudioAlbumCover()
 
-  setAudioAlbumCover() {}
+  setAudioAlbumCover(audio: GeneralPreview): GeneralPreview {
+    return Object.assign({}, audio, { albumCover: fetchAudioArt() });
+  }
 
-  setImageAlbumCover() {}
+  setImageAlbumCover(image: GeneralPreview): GeneralPreview {
+    return Object.assign({}, image, {
+      albumCover:
+        image.defaultMediaPath !== ""
+          ? fetchImageObjectFromCloudFormation(
+              image.defaultMediaPath,
+              this.editParams
+            )
+          : fetchNoMediaDefaultImage(),
+    });
+  }
 
-  setVideoAlbumCover() {}
+  setVideoAlbumCover(video: GeneralPreview): GeneralPreview {
+    return Object.assign({}, video, {
+      albumCover:
+        video.defaultMediaPath !== ""
+          ? fetchImageObjectFromCloudFormation(
+              video.defaultMediaPath,
+              this.editParams
+            )
+          : fetchVideoArt(),
+    });
+  }
+
+  openModalDialog(modalId: string, itemId: string) {
+    console.log("clicked");
+    this.store.dispatch(
+      new ModalsActions.FetchAppModal({ appModalId: "portfolio" })
+    );
+
+    if (this.componentModal) {
+      const modalToActivate = this.componentModal.modals.filter(
+        (x) => x.name === modalId
+      )[0];
+      const modalToOpen: IModal = {
+        index: modalToActivate.index,
+        name: modalToActivate.name,
+        display: ModalDisplay.table,
+        viewMode: ModalViewModel.edit,
+        contentType: modalToActivate.contentType,
+        data: modalToActivate.data,
+        modalCss: "modal aligned-modal",
+        modalDialogCss: "modal-dialog",
+        showMagnifier: false,
+      };
+      this.fetchMedia(itemId);
+
+      this.store.dispatch(
+        new ModalsActions.ToggleModal({
+          appModal: this.componentModal,
+          modal: modalToOpen,
+        })
+      );
+    }
+  }
+
+  fetchMedia(mediaId: string): void {
+    this.userStore.dispatch(new PortfolioActions.FetchMediaById({ mediaId }));
+  }
 }
